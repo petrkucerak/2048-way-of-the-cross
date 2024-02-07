509064601648fa96d02b7ad9bef46af39c814766
petrkucerak <petr@khome.cz>
2024-02-07 01:18:38 +0100

    Formate comment

--- js/progress_map.js
+++ js/progress_map.js
@@ -3,7 +3,7 @@ function updateProgress(grid) {
     2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768,
     65536, 131072, 262144,
   ];
-  //   console.log(grid);
+  // console.log(grid);
   // get max value
   let maxValue = 0;
   grid.cells.map((row) =>
